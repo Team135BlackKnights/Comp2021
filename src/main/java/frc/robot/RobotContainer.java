1114b895d73929856c94379108990c56908c1e8a
Gavin Collier <robotics@phm.k12.in.us>
2021-02-27 08:58:18 -0500

    File rename

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -62,7 +62,7 @@ public class RobotContainer implements RobotMap {
   public RobotContainer() {
       
     // Configure the button bindings
-    drive.setDefaultCommand(new mecanumDrive(drive));
+    drive.setDefaultCommand(new motorSpecificControl(drive));
     
     configureButtonBindings();
   }
